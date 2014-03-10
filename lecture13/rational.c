65cb07e9ef929c7cd5da81b14977f897764b7696
deborahhellen <hellen.deborahv@gmail.com>
2014-03-10 11:41:51 -0400

    Quiz 3

--- lecture13/rational.c
+++ lecture13/rational.c
@@ -13,6 +13,10 @@ typedef struct {
 // If unable to allocate, prints an error message and exits.
 Rational *make_rational(int numer, int denom) {
     Rational *r = malloc(sizeof(Rational));
+    if (r == NULL) {
+        fprintf(stderr, "Malloc failed");
+        exit(-1);
+    }
     r->numer = numer;
     r->denom = denom;
 
@@ -27,8 +31,10 @@ double rational_to_double(Rational *rational) {
 
 // Multiplies two rational numbers; returns a new Rational.
 Rational *mult_rational(Rational *r1, Rational *r2) {
-    // FILL THIS IN
-    return NULL;
+    Rational *mult_r = malloc(sizeof(Rational));
+    mult_r->numer = r1->numer*r2->numer;
+    mult_r->denom = r2->denom*r2->denom;
+    return mult_r;
 }
 
 // Frees a Rational.
