73493e50b20074af4ff930d400c58a38ef4d91dc
deborahhellen <hellen.deborahv@gmail.com>
2014-03-26 22:45:00 -0400

    sine wave works now

--- hw04/wave2/wave2.ino
+++ hw04/wave2/wave2.ino
@@ -12,16 +12,19 @@
  
  */
  
+#include "Waveforms.h"
  
 int ledPin = 5;       // select the pin for the LED
 int buttonPin1 = 2;
 int buttonPin2 = 3;
+volatile int wave1 = 0;
 
 void setup() {
   Serial.begin(9600);
   
   pinMode(buttonPin1, INPUT_PULLUP);  
   pinMode(buttonPin2, INPUT_PULLUP);  
+  attachInterrupt(buttonPin2, wave1Select, RISING);  // Interrupt attached to the button connected to pin 3
 
   pinMode(ledPin, OUTPUT);
   
@@ -33,28 +36,73 @@ void setup() {
   pinMode(8, OUTPUT);  
   pinMode(7, OUTPUT);  
   pinMode(6, OUTPUT);  
+  
+cli();//stop interrupts
+
+////set timer0 interrupt at 2kHz
+//  TCCR0A = 0;// set entire TCCR0A register to 0
+//  TCCR0B = 0;// same for TCCR0B
+//  TCNT0  = 0;//initialize counter value to 0
+//  // set compare match register for 2khz increments
+//  OCR0A = 124;// = (16*10^6) / (2000*64) - 1 (must be <256)
+//  // turn on CTC mode
+//  TCCR0A |= (1 << WGM01);
+//  // Set CS01 and CS00 bits for 64 prescaler
+//  TCCR0B |= (1 << CS01) | (1 << CS00);   
+//  // enable timer compare interrupt
+//  TIMSK0 |= (1 << OCIE0A);
+
+//set timer1 interrupt at 1Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS10 and CS12 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+
+////set timer2 interrupt at 8kHz
+//  TCCR2A = 0;// set entire TCCR2A register to 0
+//  TCCR2B = 0;// same for TCCR2B
+//  TCNT2  = 0;//initialize counter value to 0
+//  // set compare match register for 8khz increments
+//  OCR2A = 249;// = (16*10^6) / (8000*8) - 1 (must be <256)
+//  // turn on CTC mode
+//  TCCR2A |= (1 << WGM21);
+//  // Set CS21 bit for 8 prescaler
+//  TCCR2B |= (1 << CS21);   
+//  // enable timer compare interrupt
+//  TIMSK2 |= (1 << OCIE2A);
+
+
+sei();//allow interrupts
 }
 
 void writeByte(int x) {
-  int pin;
-  
-//  for (pin=13; pin>=6; pin--) {
-//    digitalWrite(pin, x&1);
-//    x >>= 1;
-//  }
 
-PORTD ^= B00000011;
-PORTB ^= B00111111;
+  PORTD ^= B11111110;
+  PORTB ^= B00111111;
 }
 
 int low = 36;
 int high = 255;
 int stride = 5;
 int counter = low;
+int i = 0;
 
 void loop() {
   int button1 = digitalRead(buttonPin1);
   if (button1) return;
+  
+  writeByte(waveformsTable[wave1][i]);
+
+  i++;
+  if(i == maxSamplesNum)  // Reset the counter to repeat the wave
+    i = 0;
   
   counter += stride;
   if (counter > high) {
@@ -63,5 +111,10 @@ void loop() {
   }
 
   // write to the digital pins  
-  writeByte(counter);
+  //writeByte(counter);
+}
+
+// function hooked to the interrupt on digital pin 3
+void wave1Select() {
+  wave1 = 0;
 }
